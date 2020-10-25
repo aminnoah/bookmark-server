dda0d0c1916d5598a94b38dd5ccbc426c4bca351
Noah Turner <aminnnoah19@gmail.com>
2020-10-25 13:20:48 -0500

    first commit

---
+++ src/bookmarks-router.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const logger = require('./logger');
+const { v4: uuid } = require('uuid');
+
+
+const {PORT} = require('./config');
+const bookmarks = require('./store');
+
+const bookmarksRouter = express.Router();
+const bodyParser = express.json();
+
+bookmarksRouter
+  .route('/bookmarks')
+  .get((req, res) => {
+    res.json(bookmarks);
+  })
+  .post(bodyParser, (req, res) => {
+    const { title, url, desc, rating } = req.body;
+
+    if (!title) {
+      logger.error('Title is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    if (!url) {
+      logger.error('Url is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    if (!desc) {
+      logger.error('Desc is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    if (!rating) {
+      logger.error('Rating is required');
+      return res
+        .status(400)
+        .send('Invalid data');
+    }
+
+    const id = uuid();
+
+    const bookmark = {
+      id,
+      title,
+      url,
+      desc,
+      rating
+    };
+
+    bookmarks.push(bookmark);
+
+    logger.info(`Card with id ${id} created`);
+
+    res
+      .status(201)
+      .location(`${PORT}/bookmarks/${id}`)
+      .json(bookmark);
+  });
+bookmarksRouter
+  .route('/bookmarks/:id')
+  //  GET Bookmark by Id
+  .get((req, res) => {
+
+    const { id } = req.params;
+    const bookmark = bookmarks.find(bookmark => bookmark.id === id);
+
+    if (!bookmark) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Bookmark Not Found');
+    }
+    res.json(bookmark);
+  })
+  //  DELETE Bookmark by Id
+  .delete((req, res) => {
+    const { id } = req.params;
+    const bookmarkIndex = bookmarks.findIndex(bookmark => bookmark.id === id);
+
+    if (bookmarkIndex === -1) {
+      logger.error(`Bookmark with id ${id} not found.`);
+      return res
+        .status(404)
+        .send('Not Found');
+    }
+
+    bookmarks.splice(bookmarkIndex, 1);
+
+    logger.info(`Bookmark with id ${id} deleted.)`);
+    res
+      .status(204)
+      .end();
+  });
+
+module.exports = bookmarksRouter;
